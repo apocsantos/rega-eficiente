71a3dc76cb0dc1aeba4a5adc483eca84d38d3dac
António Santos <antoniopocsantos@gmail.com>
2021-06-17 20:32:46 +0100

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+let sat = 0
+let luz = 0
+basic.forever(function () {
+    basic.clearScreen()
+    OLED.init(128, 64)
+    basic.pause(100)
+    sat = pins.analogReadPin(AnalogPin.P0)
+    luz = input.lightLevel()
+    if (sat <= 50 && luz > 70) {
+        images.iconImage(IconNames.Sad).showImage(0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        OLED.writeStringNewLine("Rega em curso")
+    } else {
+        images.iconImage(IconNames.Happy).showImage(0)
+        images.iconImage(IconNames.Umbrella).scrollImage(1, 200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        OLED.writeStringNewLine("Rega concluída, solo em conduções de humidade perfeitas")
+    }
+})
